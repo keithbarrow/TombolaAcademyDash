d2a147a53007ad36faed0f8bad990c8d6144465e
keith.barrow <keith.barrow@tombola.com>
2015-11-20 12:13:54 +0000

    Added party mode

--- .build/scripts/app.js
+++ .build/scripts/app.js
@@ -978,12 +978,20 @@
     'use strict';
     angular.module('myApp')
         .controller('MainController', ['$scope', '$state','TokenService', function($scope, $state, tokenService){
+
+            $scope.partying = function(){
+                var now = new Date();
+                return now.getDay() === 5;
+            };
+
             $scope.isAuthenticated = function(){
                 return tokenService.isAuthenticated();
             };
+
             $scope.logout = function (){
                 tokenService.resetToken();
             };
+
             $state.go('waitingPulls');
     }]);
 })();
